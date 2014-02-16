23c64872073e72ccd86ba5f93c25d2066e73752d
Stephan Spindler <stephan@spindler.priv.at>
2014-02-16 21:16:44 +0100

    Mailer uses smtp server to send emails
    configure mail server, mail user, eg. in config.php

--- email/Mailer.php
+++ email/Mailer.php
@@ -2,9 +2,13 @@
 
 /** 
  * @author martijn
- * 
+ * @author Stephan Spindler <stephan@spindler.priv.at>
  * 
  */
+require_once "class.phpmailer.php";
+require_once "class.smtp.php";
+
+
 class Mailer {
 
 	private $serverDir;
@@ -13,7 +17,43 @@ class Mailer {
 		$this->caller = "http://".$_SERVER["SERVER_NAME"].$_SERVER['REQUEST_URI'];
 		$this->caller = substr($this->caller, 0, strpos($this->caller, '?'));
 		$this->mailFrom = ' My Buddysystem <noreply@example.com>';
+                
+
 	}
+        
+        function getMail(){
+                $mail = new PHPMailer();
+                
+                $mail->IsSMTP();
+                $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+                $mail->SMTPAuth = true;
+                $mail->SMTPSecure = 'ssl';
+                $mail->Host = $GLOBALS['MAIL_SERVER'];
+                $mail->Port = $GLOBALS['MAIL_PORT'];
+                $mail->IsHTML( false );
+                $mail->Username = $GLOBALS['MAIL_USERNAME'];
+                $mail->Password = $GLOBALS['MAIL_PASSWORD'];
+                
+
+                return $mail;
+        }
+        
+        function sendMail( $address, $subject, $body ){
+            
+            $mail = $this->getMail();
+            $mail->SetFrom($GLOBALS['MAIL_FROM'] );
+            $mail->Subject = $subject ;
+            $mail->Body =  $body;
+            $mail->AddAddress( $address );
+            if( !$mail->Send() ){
+                echo "Mailer Error: " . $mail->ErrorInfo;
+                return false;
+            }else{
+                echo "Message has been sent";
+                return true;
+            }
+        }
+        
 	
 	function composeConfirmationBuddy($buddy, $smarty, $new)
 	{
@@ -23,7 +63,7 @@ class Mailer {
 
 		$message = $smarty->fetch('mail_registered_buddy_'.($new ? 'new' : 'old').'.tpl');
 		
-		mail($buddy->getEmail(),'Buddysystem registration',$message, $this->getHeader());
+		return $this->sendMail($buddy->getEmail(),'Buddysystem registration',$message);
 	}
 	
 	function composeConfirmationNewIncoming($incoming, $smarty)
@@ -34,7 +74,7 @@ class Mailer {
 
 		$message = $smarty->fetch('mail_registered_incoming.tpl');
 
-		mail($incoming->getEmail(),'Buddysystem registration',$message, $this->getHeader());
+		return $this->sendMail($incoming->getEmail(),'Buddysystem registration',$message);
 	}	
 
 	function composeConfirmationNewGroup($person, $smarty)
@@ -45,15 +85,10 @@ class Mailer {
 
 		$message = $smarty->fetch('mail_matched_'.($person['type']=='incoming' ? 'incoming' : 'buddy').'.tpl');
 
-		mail($person['email'],'Buddysystem group confirmation',$message, $this->getHeader());
+		return $this->sendMail($person['email'],'Buddysystem group confirmation',$message);
 	}
 
-	private function getHeader()
-	{
-		return
-			'From: '.$this->mailFrom."\r\n"
-			."Content-Type: text/plain; charset=\"UTF-8\"";
-	}
+
 	
 }
 
